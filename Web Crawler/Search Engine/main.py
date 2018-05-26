89cbaa0c1afd2d48406b57d2b512bfed56490200
ElaMoscicka <elakorzeniowska@interia.pl>
2018-05-26 22:32:09 +0200

    Create main.py

---
+++ Web Crawler/Search Engine/main.py
@@ -0,0 +1,258 @@
+#Completed building Search Engine.
+
+# Getting url from the Internet
+def get_page(url):
+    try:
+        import urllib
+        return urllib.urlopen(url).read()
+    except:
+        return ""
+
+# Returning first link and end position of the link
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+# Union two arrays
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+
+# Extracting all links from requested page
+def get_all_links(page):
+    links = []
+    while True:
+        url,endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+# starting web crawling on a particular url
+#  url: [list of pages url links to] 
+def crawl_web(seed): # returns index, graph of outlinks
+    tocrawl = [seed]
+    crawled = []
+    graph = {}  # <url>:[list of pages it links to]
+    index = {} 
+    while tocrawl: 
+        page = tocrawl.pop()
+        if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            outlinks = get_all_links(content)
+            graph[page] = outlinks
+            union(tocrawl, outlinks)
+            crawled.append(page)
+    return index, graph
+
+
+# Adding funtions for indexing and crawled pages
+
+# Adding word to the index
+def add_to_index(index,keyword,url):
+    if keyword in index:
+        index[keyword].append(url)
+    else:
+        # not found, add new keyword to index
+        index[keyword] = [url]
+
+# Help function that search the index for the keyword
+def lookup(index,keyword):
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
+
+# splitting page into words and adds them to index
+def add_page_to_index(index,url,content):
+    words = content.split()
+    for keyword in words:
+        add_to_index(index,keyword,url)
+
+# recording cliks on a particular link
+def record_user_click(index,keyword,url):
+    urls = lookup(index, keyword)
+    if urls:
+        for entry in urls:
+            if entry[0] == url:
+                entry[1] += 1
+                
+                
+#Additional functions:
+
+#Finishing the page ranking algorithm
+def compute_ranks(graph):
+    d = 0.8 #dumping constant
+    numloops = 40
+
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph: #Loop throught all pages
+                if page in graph[node]: #check if node links to page
+                    newrank += d * ranks[node] / len(graph[node]) #Add to new rank based on this node
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+
+#Using the page rankings to produce the best output(best-ranked page) for a given query
+def lucky_search(index, ranks, keyword):
+    return_url = '';
+    if keyword not in index:
+        return None
+    for url in index[keyword]:
+        if url in ranks:
+            if return_url != '':
+                if ranks[url] > ranks[return_url]:
+                    return_url = url
+            else:
+                return_url = url
+    return return_url
+
+#Creating list of many pages that match the query, ordered from the most likely to be useful to the least likely
+def ordered_search(index, ranks, keyword):
+    pages = lookup(index, keyword)
+    return quick_sort(pages, ranks)
+
+# Help function that uses quick sort algorithm to order array
+def quick_sort(pages, ranks):
+    if not pages or len(pages) <= 1:
+        return pages
+    else:
+        pivot = ranks[pages[0]] #find pivot
+        worse = []
+        better = []
+        for page in pages[1:]:
+            if ranks[page] <= pivot:
+                worse.append(page)
+            else:
+                better.append(page)
+    return quick_sort(better, ranks) + [pages[0]] + quick_sort(worse, ranks)
+
+
+cache = {
+   'http://udacity.com/cs101x/urank/index.html': """<html>
+<body>
+<h1>Dave's Cooking Algorithms</h1>
+<p>
+Here are my favorite recipies:
+<ul>
+<li> <a href="http://udacity.com/cs101x/urank/hummus.html">Hummus Recipe</a>
+<li> <a href="http://udacity.com/cs101x/urank/arsenic.html">World's Best Hummus</a>
+<li> <a href="http://udacity.com/cs101x/urank/kathleen.html">Kathleen's Hummus Recipe</a>
+</ul>
+
+For more expert opinions, check out the
+<a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a>
+and <a href="http://udacity.com/cs101x/urank/zinc.html">Zinc Chef</a>.
+</body>
+</html>
+
+""",
+   'http://udacity.com/cs101x/urank/zinc.html': """<html>
+<body>
+<h1>The Zinc Chef</h1>
+<p>
+I learned everything I know from
+<a href="http://udacity.com/cs101x/urank/nickel.html">the Nickel Chef</a>.
+</p>
+<p>
+For great hummus, try
+<a href="http://udacity.com/cs101x/urank/arsenic.html">this recipe</a>.
+
+</body>
+</html>
+
+
+""",
+   'http://udacity.com/cs101x/urank/nickel.html': """<html>
+<body>
+<h1>The Nickel Chef</h1>
+<p>
+This is the
+<a href="http://udacity.com/cs101x/urank/kathleen.html">
+best Hummus recipe!
+</a>
+
+</body>
+</html>
+
+""",
+   'http://udacity.com/cs101x/urank/kathleen.html': """<html>
+<body>
+<h1>
+Kathleen's Hummus Recipe
+</h1>
+<p>
+
+<ol>
+<li> Open a can of garbonzo beans.
+<li> Crush them in a blender.
+<li> Add 3 tablesppons of tahini sauce.
+<li> Squeeze in one lemon.
+<li> Add salt, pepper, and buttercream frosting to taste.
+</ol>
+
+</body>
+</html>
+
+""",
+   'http://udacity.com/cs101x/urank/arsenic.html': """<html>
+<body>
+<h1>
+The Arsenic Chef's World Famous Hummus Recipe
+</h1>
+<p>
+
+<ol>
+<li> Kidnap the <a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a>.
+<li> Force her to make hummus for you.
+</ol>
+
+</body>
+</html>
+
+""",
+   'http://udacity.com/cs101x/urank/hummus.html': """<html>
+<body>
+<h1>
+Hummus Recipe
+</h1>
+<p>
+
+<ol>
+<li> Go to the store and buy a container of hummus.
+<li> Open it.
+</ol>
+
+</body>
+</html>
+
+""",
+}
+
+
+index, graph = crawl_web('http://udacity.com/cs101x/urank/index.html')
+ranks = compute_ranks(graph)
+
+print ordered_search(index, ranks, 'Hummus')
+#>>> ['http://udacity.com/cs101x/urank/kathleen.html',
+#    'http://udacity.com/cs101x/urank/nickel.html',
+#    'http://udacity.com/cs101x/urank/arsenic.html',
+#    'http://udacity.com/cs101x/urank/hummus.html',
+#    'http://udacity.com/cs101x/urank/index.html']
